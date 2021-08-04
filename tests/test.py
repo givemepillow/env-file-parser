98308324293049282335616b39a7db2a3831c466
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-04 20:07:20 +0300

    added new test - test_multi_string_vars this test does not pass

--- tests/test.py
+++ tests/test.py
@@ -9,12 +9,19 @@ class TestGetEnv(unittest.TestCase):
         with open('.env', "w") as file:
             file.writelines(['''CONST="abc-123"\n''', "NUMBER = 198"])
 
+        with open('.env.multi', "w") as file:
+            file.writelines(['''STR1="abc-123"\n''', "STR2 = mixer\n", '''STR3="3246gt8456$%$^%"\n'''])
+
         with open('.env.conf', "w") as file:
             file.writelines(["CONST=adadsaskdjald\n", "NUMBER=-78"])
 
     def test_string_vars(self):
         self.assertEqual(get_env('CONST', file_path=f"{os.getcwd()}\.env"), 'abc-123')
         self.assertEqual(get_env('CONST', file_path=f"{os.getcwd()}\.env.conf"), 'adadsaskdjald')
+
+    def test_multi_string_vars(self):
+        self.assertEqual(get_env('STR1', 'STR2', 'STR3', file_path=f"{os.getcwd()}\.env.multi"),
+                         ['abc-123', 'mixer', '3246gt8456$%$^%'])
 
     def test_numbers_vars(self):
         self.assertEqual(get_env('NUMBER', file_path=f"{os.getcwd()}\.env"), 198)
@@ -23,6 +30,7 @@ class TestGetEnv(unittest.TestCase):
     def tearDown(self):
         os.remove(f"{os.getcwd()}\.env")
         os.remove(f"{os.getcwd()}\.env.conf")
+        os.remove(f"{os.getcwd()}\.env.multi")
 
 if __name__ == '__main__':
     unittest.main()
