78f147a83e0df12e99a933bca30ccc0ac8e07913
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-04 23:13:21 +0300

    get_env is divided into two functions read_env - read lines frm file, parse_env - extract vars. now get_env - for one str param with one return value, and get_envs for variable-length argument with list as a return value.

--- source/envfileparser/env_file_parser.py
+++ source/envfileparser/env_file_parser.py
@@ -3,10 +3,10 @@ import sys
 
 from .type_checker import is_int
 
-__all__ = ['get_env']
+__all__ = ['get_env', 'get_envs']
 
 
-def get_env(*const_names: str, file_path=".env"):
+def read_env(file_path):
     try:
         if not os.path.isabs(file_path):
             if sys.platform == "win32":
@@ -18,10 +18,11 @@ def get_env(*const_names: str, file_path=".env"):
             env_file_lines = env_file.readlines()
     except FileNotFoundError:
         raise FileNotFoundError(f"The {file_path} not found!")
+    return env_file_lines
 
-    env_consts = {}
-    config_vars = []
 
+def parse_env(env_file_lines):
+    env_vars = {}
     for line in env_file_lines:
         equal_index = line.index('=')
         value = line[equal_index + 1::].strip()
@@ -30,16 +31,32 @@ def get_env(*const_names: str, file_path=".env"):
         elif value[0] == value[-1] and value[0] in ('''"''', """'"""):
             value = value[1:len(value) - 1]
         key = line[0:equal_index].strip()
-        env_consts[key] = value
+        env_vars[key] = value
+    return env_vars
+
+
+def get_env(var_name: str, file_path=".env"):
+    env_file_lines = read_env(file_path)
+    env_vars = parse_env(env_file_lines)
     try:
-        for name in const_names:
-            config_vars.append(env_consts[name])
+        var = env_vars[var_name]
     except KeyError:
-        raise KeyError(f"The constant you specified is not contained in the {file_path}.")
-    if len(config_vars) == 1:
-        return config_vars[0]
-    else:
-        return config_vars
+        raise KeyError(f"{var_name} is not found in {file_path}.")
+    return var
+
+
+def get_envs(*var_names: str, file_path=".env"):
+    var_list = []
+    env_file_lines = read_env(file_path)
+    env_vars = parse_env(env_file_lines)
+    try:
+        var = None
+        for name in var_names:
+            var = env_vars[name]
+            var_list.append(var)
+    except KeyError:
+        raise KeyError(f"{var} is not found in {file_path}.")
+    return var_list
 
 
 if __name__ == "__main__":
