ffc1f0c7ab49525bc0e550062f2ee6a4ec00cc4d
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-04 20:02:45 +0300

    added tearDown and setUp methods in unittest

--- tests/test.py
+++ tests/test.py
@@ -3,22 +3,27 @@ import unittest
 
 from source import get_env
 
+
 class TestGetEnv(unittest.TestCase):
+    def setUp(self):
+        with open('.env', "w") as file:
+            file.writelines(['''CONST="abc-123"\n''', "NUMBER = 198"])
+
+        with open('.env.conf', "w") as file:
+            file.writelines(["CONST=adadsaskdjald\n", "NUMBER=-78"])
+
     def test_string_vars(self):
-        self.assertEqual(get_env('CONST'), 'abc-123')
-        self.assertEqual(get_env('CONST', file_path='.env.conf'), 'adadsaskdjald')
+        self.assertEqual(get_env('CONST', file_path=f"{os.getcwd()}\.env"), 'abc-123')
+        self.assertEqual(get_env('CONST', file_path=f"{os.getcwd()}\.env.conf"), 'adadsaskdjald')
 
     def test_numbers_vars(self):
-        self.assertEqual(get_env('NUMBER'), 198)
-        self.assertEqual(get_env('NUMBER', file_path='.env.conf'), -78)
+        self.assertEqual(get_env('NUMBER', file_path=f"{os.getcwd()}\.env"), 198)
+        self.assertEqual(get_env('NUMBER', file_path=f"{os.getcwd()}\.env.conf"), -78)
 
+    def tearDown(self):
+        os.remove(f"{os.getcwd()}\.env")
+        os.remove(f"{os.getcwd()}\.env.conf")
 
 if __name__ == '__main__':
-    with open('.env', "w") as file:
-        file.writelines(['''CONST="abc-123"\n''', "NUMBER = 198"])
-
-    with open('.env.conf', "w") as file:
-        file.writelines(["CONST=adadsaskdjald\n", "NUMBER=-78"])
     unittest.main()
-    os.remove('.env.conf')
-    os.remove('.env')
+
