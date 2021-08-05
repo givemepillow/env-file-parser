b7f881273e79304dc49228e6e97f377baa23d942
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-05 19:08:33 +0300

    Comment-description of the function read_env().

--- source/envfileparser/env_file_parser.py
+++ source/envfileparser/env_file_parser.py
@@ -7,16 +7,28 @@ __all__ = ['get_env', 'get_envs']
 
 
 def read_env(file_path):
+    """
+    This function is intended for use inside the package.
+    As a parameter, it takes a string - the path to the file
+    that will be opened in the future in a way that
+    depends on the platform used. After opening the file,
+    the data is read line by line and saved as a spike.
+    At the end of this, the list returns to the outside.
+    :param file_path: str
+    :return: env_file_lines: list
+    """
     try:
         if not os.path.isabs(file_path):
             if sys.platform == "win32":
                 file_path = os.path.abspath(os.getcwd() + '/' + file_path)
             else:
                 file_path = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), file_path))
+
         with open(file_path, 'r') as env_file:
             env_file_lines = env_file.readlines()
     except FileNotFoundError:
         raise FileNotFoundError(f"The {file_path} not found!")
+
     return env_file_lines
 
 
