2f3f76c3263632de9f9d3ee2ac38d9f8a543e440
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-05 19:31:57 +0300

    Correction of the read_env() docstring.

--- source/envfileparser/env_file_parser.py
+++ source/envfileparser/env_file_parser.py
@@ -7,15 +7,18 @@ __all__ = ['get_env', 'get_envs']
 
 
 def read_env(file_path: str) -> list:
-    """
+    """A function that reads a file with variables.
+
     This function is intended for use inside the package.
     As a parameter, it takes a string - the path to the file
     that will be opened in the future in a way that
     depends on the platform used. After opening the file,
     the data is read line by line and saved as a spike.
     At the end of this, the list returns to the outside.
-    :param file_path: str
-    :return: env_file_lines: list
+
+    :param file_path: file path
+    :type file_path: str
+    :return: list of file lines
     """
     try:
         if not os.path.isabs(file_path):
