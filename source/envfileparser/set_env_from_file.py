72a0bd9fc7ffb8541eaaf4a18f203821aa8fbfd5
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-06 23:14:20 +0300

    Added one required argument (target variable).

--- source/envfileparser/set_env_from_file.py
+++ source/envfileparser/set_env_from_file.py
@@ -3,7 +3,7 @@ import os
 from .get_env_from_file import get_env_from_file
 
 
-def set_env_from_file(*var_names: str, file_path=".env"):
+def set_env_from_file(var: str, *next_vars: str, file_path=".env"):
     file_vars = get_env_from_file(file_path=file_path)
-    for v in var_names:
+    for v in var, *next_vars:
         os.environ[v] = file_vars[v]
