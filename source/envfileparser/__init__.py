b41b9aa22630e4319c8fe55fb1008ab28c6d095e
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-06 20:05:16 +0300

    Remove get_envs import.

--- source/envfileparser/__init__.py
+++ source/envfileparser/__init__.py
@@ -1,5 +1,4 @@
 from .get_env import get_env
-from .get_envs import get_envs
 from .get_env_from_file import get_env_from_file
 
-__all__ = ['get_env', 'get_envs', 'get_env_from_file']
+__all__ = ['get_env', 'get_env_from_file']
