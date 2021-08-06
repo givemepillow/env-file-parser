81e016914bbb1e79d7123e2a1e2cbcf38d8bc47f
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-06 20:14:38 +0300

    Tests optimization.

--- tests/test_get_env_from_file.py
+++ tests/test_get_env_from_file.py
@@ -6,25 +6,21 @@ from source import get_env_from_file
 
 class TestGetEnvFromFile(unittest.TestCase):
     def setUp(self):
-        with open('.env1', "w") as file:
+        with open('.env', "w") as file:
             file.writelines(['''STR1="abc-123"\n\n''', "STR2 = mixer\n\n\n", '''STR3="3246gt8456$%$^%" # hello\n'''])
+            file.writelines(['''NUM1=-123\n\n''', "NUM2 = 118\n", '''NUM3=3246\n'''])
 
         with open('.env.empty', "w") as file:
             file.writelines("")
 
-        with open('.env2', "w") as file:
-            file.writelines(['''NUM1=-123\n\n''', "NUM2 = 118\n", '''NUM3=3246\n'''])
-
     def test_var(self):
-        self.assertEqual(get_env_from_file(file_path=".env1"),
-                         {'STR1': 'abc-123', 'STR2': 'mixer', 'STR3': '3246gt8456$%$^%'})
-        self.assertEqual(get_env_from_file(file_path=".env2"),
-                         {'NUM1': '-123', 'NUM2': '118', 'NUM3': '3246'})
+        self.assertEqual(get_env_from_file(file_path=".env"),
+                         {'STR1': 'abc-123', 'STR2': 'mixer', 'STR3': '3246gt8456$%$^%',
+                          'NUM1': '-123', 'NUM2': '118', 'NUM3': '3246'})
 
     def test_empty_file(self):
         self.assertEqual(get_env_from_file(file_path=".env.empty"), {})
 
     def tearDown(self):
-        os.remove(".env1")
-        os.remove(".env2")
+        os.remove(".env")
         os.remove(".env.empty")
