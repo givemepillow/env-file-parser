faef0eab10bf5a3098915839e4a1d311088ed480
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-06 22:32:40 +0300

    Tests for set_env_from_file().

---
+++ tests/test_set_env_from_file.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+
+from source import set_env_from_file, get_env
+
+
+class TestGetEnvFromFile(unittest.TestCase):
+    def setUp(self):
+        with open('.env', "w") as file:
+            file.write(
+                "# env vars to set\n"
+                "MY_VAR = 'test_var'\n"
+                "\n\n"
+                "MY_VAR_ONE = '1' # one\n"
+                "MY_VAR_TWO = 2 ## two\n"
+            )
+
+    def test_set_vars(self):
+        set_env_from_file('MY_VAR', 'MY_VAR_ONE', 'MY_VAR_TWO')
+        self.assertTrue(get_env('MY_VAR', file_path=".env"), os.environ.get('MY_VAR'))
+        self.assertTrue(get_env('MY_VAR', file_path=".env"), os.environ.get('MY_VAR_ONE'))
+        self.assertTrue(get_env('MY_VAR', file_path=".env"), os.environ.get('MY_VAR_TWO'))
+
+    def tearDown(self):
+        os.environ.pop('MY_VAR_ONE')
+        os.environ.pop('MY_VAR_TWO')
+        os.environ.pop('MY_VAR')
+        os.remove(".env")
