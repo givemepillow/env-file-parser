c27ed7f57a92db8a4dea3c4f9341991eb16b016b
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-06 22:30:26 +0300

    Realization of set_env_from_file().

--- source/envfileparser/set_env_from_file.py
+++ source/envfileparser/set_env_from_file.py
@@ -1,5 +1,9 @@
+import os
+
 from .get_env_from_file import get_env_from_file
 
 
 def set_env_from_file(*var_names: str, file_path=".env"):
     file_vars = get_env_from_file(file_path=file_path)
+    for v in var_names:
+        os.environ[v] = file_vars[v]
