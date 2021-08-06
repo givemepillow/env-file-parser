ae98e4717da83b3127d4c1f3d8af97f6b906838b
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-06 22:31:28 +0300

    Import of set_env_from_file().

--- source/envfileparser/__init__.py
+++ source/envfileparser/__init__.py
@@ -1,4 +1,5 @@
 from .get_env import get_env
 from .get_env_from_file import get_env_from_file
+from .set_env_from_file import set_env_from_file
 
-__all__ = ['get_env', 'get_env_from_file']
+__all__ = ['get_env', 'get_env_from_file', 'set_env_from_file']
