d7b54b525a63b6a9f43096bd0d06ff847b8793dd
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-06 20:02:38 +0300

    Refactor. Deprecated.

--- source/envfileparser/get_env.py
+++ source/envfileparser/get_env.py
@@ -1,9 +1,8 @@
-from .parse_env import parse_env
-from .read_env import read_env
-
+from .get_env_from_file import get_env_from_file
 
 def get_env(var_name: str, file_path=".env") -> str:
     """A function that returns value of the specified variables as a string.
+    Deprecated! Left for compatibility support.
 
     The var name is passed to the function as a string.
     and a named parameter - the path to the file with a default value.
@@ -13,11 +12,9 @@ def get_env(var_name: str, file_path=".env") -> str:
     :param file_path: the string is the path to the file, it has a default value
     :return: value of extracted var as a string
     """
-    env_file_lines = read_env(file_path)
-    env_vars = parse_env(env_file_lines)
 
     try:
-        var = env_vars[var_name]
+        var = get_env_from_file(file_path=file_path)[var_name]
     except KeyError:
         raise KeyError(f"{var_name} is not found in {file_path}.")
 
