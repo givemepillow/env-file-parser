09b9fb2123125a76ee56afefc13c284ab615b179
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-08 09:25:29 +0300

    Added KetError exception if key is exists. Works only if override is false. By default override - False.

--- source/envfileparser/set_envs_from_file.py
+++ source/envfileparser/set_envs_from_file.py
@@ -3,8 +3,18 @@ import os
 from .get_envs_from_file import get_envs_from_file
 
 
-def set_envs_from_file(key: str, *next_keys: str, file_path: str = ".env", override: bool = True) -> None:
+def set_envs_from_file(key: str, *next_keys: str, file_path: str = ".env", override: bool = False) -> None:
+    """
+
+    :param key: str: one required argument is the variable name
+    :param next_keys: str: an optional argument is a sequence of names
+    :param file_path: str: the file path
+    :param override: bool: flag for overwriting environment variables
+    :return:
+    """
     file_vars = get_envs_from_file(file_path=file_path)
     for v in key, *next_keys:
-        if override or os.environ.get(v, '__exists__') == '__exists__':
+        if override or os.environ.get(v, '__not_exists__') == '__not_exists__':
             os.environ[v] = file_vars[v]
+        else:
+            raise KeyError(f"Key {v} is already exists in environment!")
