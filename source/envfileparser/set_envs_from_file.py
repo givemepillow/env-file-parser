f4ac05d5da5b732203084c43da129cf0ee4b5efb
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-08 09:17:27 +0300

    Updated args types marks.

--- source/envfileparser/set_envs_from_file.py
+++ source/envfileparser/set_envs_from_file.py
@@ -3,7 +3,7 @@ import os
 from .get_envs_from_file import get_envs_from_file
 
 
-def set_envs_from_file(key: str, *next_keys: str, file_path=".env", override=True):
+def set_envs_from_file(key: str, *next_keys: str, file_path: str = ".env", override: bool = True) -> None:
     file_vars = get_envs_from_file(file_path=file_path)
     for v in key, *next_keys:
         if override or os.environ.get(v, '__exists__') == '__exists__':
