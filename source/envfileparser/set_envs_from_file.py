f783198c02d179d3d88c7e62e10afb39b1f664d1
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-08 09:14:50 +0300

    Added override flag.

--- source/envfileparser/set_envs_from_file.py
+++ source/envfileparser/set_envs_from_file.py
@@ -3,7 +3,8 @@ import os
 from .get_envs_from_file import get_envs_from_file
 
 
-def set_envs_from_file(key: str, *next_keys: str, file_path=".env"):
+def set_envs_from_file(key: str, *next_keys: str, file_path=".env", override=True):
     file_vars = get_envs_from_file(file_path=file_path)
     for v in key, *next_keys:
-        os.environ[v] = file_vars[v]
+        if override or os.environ.get(v, 'exists') == 'exists':
+            os.environ[v] = file_vars[v]
