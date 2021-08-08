2e9fae853bd0a000801400384b2c1d5f6b8ac630
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-08-08 09:28:09 +0300

    Added docstring.

--- source/envfileparser/set_envs_from_file.py
+++ source/envfileparser/set_envs_from_file.py
@@ -4,13 +4,16 @@ from .get_envs_from_file import get_envs_from_file
 
 
 def set_envs_from_file(key: str, *next_keys: str, file_path: str = ".env", override: bool = False) -> None:
-    """
+    """Reads and analyzes the file, and then sets the found variables in the environment.
+
+    The function accepts a variable number of keys,
+    a path, and a flag for overwriting already existing environment variables.
 
     :param key: str: one required argument is the variable name
     :param next_keys: str: an optional argument is a sequence of names
     :param file_path: str: the file path
     :param override: bool: flag for overwriting environment variables
-    :return:
+    :return: None
     """
     file_vars = get_envs_from_file(file_path=file_path)
     for v in key, *next_keys:
